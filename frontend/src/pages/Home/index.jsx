b6e43883044782c2672c80390e7cccf954bb3b39
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-19 10:13:43 +0800

    added responsiveness to Homepage

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -7,10 +7,10 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen px-36 py-8">
-      <ul className="flex items-center justify-between">
-        <li className="text-h6 flex gap-4">
-          <div className="w-8 h-8 rounded-full bg-gradientBlue-start"></div>
+    <div className="flex h-screen flex-col p-4 lg:px-36 lg:py-8 ">
+      <ul className="flex flex-row items-center justify-between">
+        <li className="flex gap-4 text-h6">
+          <div className="h-8 w-8 rounded-full bg-gradientBlue-start"></div>
           Something
         </li>
         <li className="bg-slate-100 ">
@@ -18,23 +18,22 @@ export default function Home() {
         </li>
       </ul>
 
-      <div className="item-center flex h-full justify-around">
-        <div className="flex w-1/2 flex-col justify-center gap-8 ">
+      <div className="item-center flex grow flex-col-reverse justify-evenly lg:flex-row">
+        <div className="flex flex-col justify-center gap-8 text-center lg:text-left">
           <h2>Lorem ipsum dolor sit amet</h2>
           <h5>
             onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua.
           </h5>
-          <div className="w-52">
+          <div className="w-52 self-center lg:self-start">
             <Button
               text="Let's Go"
               bgcolor="bg-gradient-to-r from-gradientBlue-start to-gradientBlue-end"
             />
-          </div>
+          </div>  
         </div>
-
-        <div className="m-auto w-1/2">
-          <img src={DesignTeamwork} alt="team work svg" />
+        <div className="lg:m-auto">
+          <img src={DesignTeamwork} alt="team work svg" class="shrink"/>
         </div>
       </div>
     </div>
