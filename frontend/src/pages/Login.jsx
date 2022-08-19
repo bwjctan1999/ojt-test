fe4e22b4e8dbeb81243f21fe95a7e189db414cb6
Rio Gwyneth Soliva <20170012516@my.xu.edu.ph>
2022-08-19 09:09:40 -1200

    changed login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,21 +4,44 @@ import TextField from "../components/Textfield";
 
 export default function Login() {
   return (
-    <div className="flex h-screen w-screen flex-col-reverse bg-BGblue lg:flex-row">
-      <div className="flex h-3/5 w-full max-w-lg flex-col justify-center gap-6 rounded-2xl bg-white p-4 lg:h-auto lg:p-12">
-        <h4 className="gap-6 font-bold text-black">Login</h4>
-        <TextField placeholder="Email" type="email" />
-        <TextField placeholder="Password" type="password" />
-        <a className="ml-auto block">Forgot Password?</a>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gradientBlue-start to-gradientBlue-end">
+    <div className="w-2/5 px-16 pt-12 pb-10 text-left bg-white shadow-lg rounded-xl">
 
-        <Button
-          text="Login"
-          bgcolor="bg-gradient-to-r from-gradientBlue-start to-gradientBlue-end"
-        />
-      </div>
-      <div className="flex h-2/5 grow justify-center lg:h-full lg:p-28">
-        <img src={DesignTeamwork} alt="teamwork svg" />
-      </div>
+        <h3 className="text-center text-2xl text-zinc-900 font-bold  mb-4 pb-3 ">Login</h3>
+      
+        <form action="">
+            <div className="mt-4">
+                <div>
+                            <input type="text" placeholder="Email"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                            
+                </div>
+                <div className="mt-4 text-right">
+        
+                            <input type="password" placeholder="Password"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 mb-2"/>
+                </div>
+
+                <div className="text-center"> 
+                     <input type="checkbox"/><label class="text-sm mr-44"> Remember me</label>
+                     <a href="#" 
+                     className=" text-sm my-5 text-blue-600 hover:underline ">Forget Password?</a>
+                </div>
+
+                <div className="flex items-baseline justify-between mt-8">
+                <Button
+                onClick={() => navigate("/Login")}
+                text="Login"
+                bgcolor="bg-gradient-to-r from-gradientBlue-start to-gradientBlue-end"
+              />
+                </div>
+
+                <div className="text-center">
+                <p className="text-sm my-5">Don't have an account? <a href="#" className="text-blue-600 hover:underline">Sign up</a></p>
+                </div>
+            </div>
+        </form>
     </div>
+</div>
   );
 }
