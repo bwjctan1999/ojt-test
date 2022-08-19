1e389824f5756560ac69e4b32be009b8f185ce50
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-19 14:45:35 +0800

    added Store page and StoreCard

---
+++ frontend/src/pages/Store/StoreCard.jsx
@@ -0,0 +1,16 @@
+export default function StoreCard({ title, desc }) {
+  return (
+    <div className="h-full w-full rounded-lg shadow-md">
+      <div className="h-2/5 w-full rounded-t-lg bg-slate-500 pl-6 pt-6 pb-14 pr-14">
+        <div className="h-12 w-12 rounded-lg bg-white"></div>
+      </div>
+      <div className="px-6 py-4">
+        <h6 className="text-s1 font-medium">{title}</h6>
+        <p className="text-b1">
+          {desc}
+          asdfasf
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Store/index.jsx
@@ -0,0 +1,59 @@
+import LoginButton from "../../components/LoginButton";
+import StoreCard from "./StoreCard";
+
+export default function Store() {
+  const tests = [
+    {
+      title: "Janitorial Service",
+      desc: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    },
+    {
+      title: "House Keeping",
+      desc: "uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+    {
+      title: "Air Conditioner Cleaning",
+      desc: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    },
+    {
+      title: "Cook",
+      desc: "uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    },
+    {
+      title: "Electronics Repair",
+      desc: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+    },
+    {
+      title: "Something I dunno",
+      desc: "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Blabla Blablabla",
+      desc: "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ];
+
+  const fillCards = () => {
+    return tests.map(({ title, desc }) => (
+      <StoreCard title={title} desc={desc} />
+    ));
+  };
+
+  return (
+    <div className="flex h-screen flex-col gap-8 p-4 lg:px-36 lg:py-8 ">
+      <ul className="flex flex-row items-center justify-between">
+        <li className="flex gap-4 text-h6">
+          <div className="h-8 w-8 rounded-full bg-gradientBlue-start"></div>
+          Something
+        </li>
+        <li className="bg-slate-100 ">
+          <LoginButton />
+        </li>
+      </ul>
+
+      <div className="grid grow grid-cols-1 gap-5 lg:grid-cols-5">
+        {fillCards()}
+      </div>
+    </div>
+  );
+}
