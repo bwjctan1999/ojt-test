68020e5b03cd03241de135dd37c76eb7ebe3ce1c
Rio Gwyneth Soliva <20170012516@my.xu.edu.ph>
2022-08-19 13:10:27 -1200

    changes in border and shadow

--- frontend/src/components/Textfield.jsx
+++ frontend/src/components/Textfield.jsx
@@ -3,7 +3,7 @@ export default function TextField({type, placeholder}) {
         <input 
             type={type} 
             placeholder={placeholder} 
-            className="p-4 bg-white w-full rounded-2xl shadow text-black outline-none"
+            className="p-4 bg-white w-full border border-opacity-50 rounded-2xl shadow text-black outline-none"
         />
     );
 }
