b46fe610841c3e6ffccaad100def5ec63849f746
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-22 13:22:30 +0800

    added Product.jsx

---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,18 @@
+export default function Product() {
+  return (
+    <div className="flex">
+      <div className="flex flex-col"></div>
+      <div>
+        <h3>Lasagna</h3>
+        <p>Dessert</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus tempor
+          sed commodo, scelerisque. Adipiscing bibendum pretium habitasse eget
+          nisl tellus et amet nam. Ut quis et auctor vel, nullam tristique sit
+          est. Et netus eu malesuada a quisque. Senectus vitae sagittis
+          malesuada facilisis torto.
+        </p>
+      </div>
+    </div>
+  );
+}
