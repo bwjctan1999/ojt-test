2381d3b8a5cf9dd103a0d7a3f3ab19457335149c
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-22 13:09:07 +0800

    fixed aspect ratio of card

--- frontend/src/pages/Store/StoreCard.jsx
+++ frontend/src/pages/Store/StoreCard.jsx
@@ -1,14 +1,12 @@
 export default function StoreCard({ title, desc }) {
   return (
     <div className="h-full w-full rounded-lg shadow-md">
-      <div className="h-2/5 w-full rounded-t-lg bg-slate-500 pl-6 pt-6 pb-14 pr-14">
-        <div className="h-12 w-12 rounded-lg bg-white"></div>
+      <div className="aspect-video w-full rounded-t-lg bg-slate-500 pl-6 pt-6 pb-14 pr-14">
+        <div className="aspect-square w-1/3 rounded-lg bg-white"></div>
       </div>
       <div className="px-6 py-4">
         <h6 className="text-h6 font-medium">{title}</h6>
-        <p className="text-s1 mt-2">
-          {desc}
-        </p>
+        <p className="mt-2 text-s1">{desc}</p>
       </div>
     </div>
   );
