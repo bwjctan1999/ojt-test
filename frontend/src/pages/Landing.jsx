69fd416f15c2819d2d5a305cebbc7dc06921ae8e
Rio Gwyneth Soliva <20170012516@my.xu.edu.ph>
2022-08-23 08:21:09 -1200

    dfgd

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -8,11 +8,23 @@ export default function Landing() {
 
 <div> 
 <Navbar/>
-<div className="flex flex-wrap  bg-gray-50 px-32">
+<div className="flex flex-wrap gap-x-64git bg-gray-50 px-32">
 
     
 
-    <div className="">
+    <div className="basis-96">
+        <h4 className="text-sm">home/dessert/lasagna</h4>
+       
+
+
+        <div className="w-auto">
+        <p className="text-sm text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus tempor sed commodo, scelerisque. Adipiscing bibendum pretium habitasse eget nisl tellus et amet nam. Ut quis et auctor vel, nullam tristique sit est. Et netus eu malesuada a quisque. Senectus vitae sagittis malesuada facilisis torto.
+        </p>
+        </div>
+    </div>
+
+    <div className="basis-96">
         <h4 className="text-sm">home/dessert/lasagna</h4>
        
 
